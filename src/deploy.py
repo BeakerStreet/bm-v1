a227ee9ff82666338b0213aed68d45f2e777aa90
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-01-24 17:43:59 +0000

    adds deploy.py with a Deploy class to put the model into SageMaker

--- src/deploy.py
+++ src/deploy.py
@@ -2,33 +2,34 @@ import logging
 import boto3
 import sagemaker
 from sagemaker.tensorflow import TensorFlowModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 class Deploy:
-    def __init__(self, bucket_name, role, framework_version='2.3.0'):
+    def __init__(self, bucket_name, role, framework_version='2.18.0'):
         self.s3 = boto3.client('s3')
-        self.bucket_name = bucket_name
-        self.role = role
+        self.bucket_name = os.getenv('MODEL_BUCKET_NAME')
+        self.role = os.getenv('AWS_IAM_ROLE')
         self.framework_version = framework_version
-        self.logger = self.setup_logging()
+        self.model_file = 'model.keras'
 
-    def setup_logging(self):
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-        return logging.getLogger(__name__)
-
-    def upload_model_to_s3(self, model_file):
-        self.logger.info(f"Uploading {model_file} to S3 bucket {self.bucket_name}")
-        self.s3.upload_file(model_file, self.bucket_name, model_file)
+    def upload_to_s3(self):
+        self.logger.info(f"Uploading {self.model_file} to S3 bucket {self.bucket_name}")
+        self.s3.upload_file(self.model_file, self.bucket_name, self.model_file)
         self.logger.info("Upload complete")
 
-    def deploy_model(self, model_file):
+    def deploy_model(self):
         self.logger.info("Creating SageMaker model and deploying endpoint")
         sagemaker_session = sagemaker.Session()
         model = TensorFlowModel(
-            model_data=f's3://{self.bucket_name}/{model_file}',
+            model_data=f's3://{self.bucket_name}/{self.model_file}',
             role=self.role,
             framework_version=self.framework_version,
             sagemaker_session=sagemaker_session
