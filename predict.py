fa54c5980971728a76445b586f31eb4ad0d3ddc0
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-01-27 18:19:42 +0000

    rn some functions, rm logging

--- predict.py
+++ predict.py
@@ -1,18 +1,13 @@
 import logging
 import boto3
 import numpy as np
-from src.dataset import Dataset
 from tensorflow.keras.models import load_model
 import sys
 import sagemaker
 from sagemaker.tensorflow import TensorFlowModel
+
 from src.deploy import Deploy
-from dotenv import load_dotenv
-import os
-
-load_dotenv(dotenv_path='.env')
-
-s3 = boto3.client('s3')
+from src.dataset import Dataset
 
 def setup_logging(): # do we want this? Or should it be in the deploy.py file?
     logging.basicConfig(
@@ -21,31 +16,6 @@ def setup_logging(): # do we want this? Or should it be in the deploy.py file?
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     return logging.getLogger(__name__)
-
-def upload_model_to_s3(model_file, bucket_name): # move to deploy.py?
-    logger = setup_logging()
-    logger.info(f"Uploading {model_file} to S3 bucket {bucket_name}")
-    s3.upload_file(model_file, bucket_name, model_file)
-    logger.info("Upload complete")
-
-def deploy_model_to_sagemaker(bucket_name, model_file, role): # move to deploy.py?
-    logger = setup_logging()
-    logger.info("Creating SageMaker model and deploying endpoint")
-
-    sagemaker_session = sagemaker.Session()
-    model = TensorFlowModel(
-        model_data=f's3://{bucket_name}/{model_file}',
-        role=role,
-        framework_version='2.3.0',
-        sagemaker_session=sagemaker_session
-    )
-
-    predictor = model.deploy(
-        initial_instance_count=1,
-        instance_type='ml.m5.large'
-    )
-    logger.info("Deployment complete")
-    return predictor
 
 def train():
     logger = setup_logging()
@@ -72,34 +42,17 @@ def predict():
     logger = setup_logging()
     logger.info("=== Predicting ===")
 
-    MODEL_BUCKET_NAME = os.getenv('MODEL_BUCKET_NAME')
-    AWS_IAM_ROLE = os.getenv('AWS_IAM_ROLE')
-
-    # Load the dataset for prediction
+    # Load dataset
     dataset = Dataset(dataset_path='data/dataset.json', image_folder='data/assets')
 
-    # Initialize Deploy class
-    deployer = Deploy(MODEL_BUCKET_NAME, AWS_IAM_ROLE)
+    # Initialize Deploy
+    deployer = Deploy()
 
-    # Upload model to S3
-    logger.info(f"Uploading {deployer.model_file} to S3 bucket {deployer.bucket_name}")
-    deployer.s3.upload_file(deployer.model_file, deployer.bucket_name, deployer.model_file)
-    logger.info("Upload complete")
+    # Upload model to S3 using Deploy class method
+    deployer.s3_upload()
 
-    # Deploy model to SageMaker
-    logger.info("Creating SageMaker model and deploying endpoint")
-    sagemaker_session = sagemaker.Session()
-    model = TensorFlowModel(
-        model_data=f's3://{deployer.bucket_name}/{deployer.model_file}',
-        role=deployer.role,
-        framework_version=deployer.framework_version,
-        sagemaker_session=sagemaker_session
-    )
-    predictor = model.deploy(
-        initial_instance_count=1,
-        instance_type='ml.m5.large'
-    )
-    logger.info("Deployment complete")
+    # Deploy model to SageMaker using Deploy class method
+    predictor = deployer.deploy()
 
     # Make a prediction
     logger.info("Invoking SageMaker endpoint for prediction")
@@ -109,9 +62,7 @@ def predict():
     print(prediction)
 
     # Clean up
-    logger.info("Deleting SageMaker endpoint")
-    predictor.delete_endpoint()
-    logger.info("Endpoint deleted")
+    deployer.delete_endpoint(predictor)
 
 def main():
     if len(sys.argv) < 2:
--- src/deploy.py
+++ src/deploy.py
@@ -1,32 +1,47 @@
-import logging
 import boto3
 import sagemaker
 from sagemaker.tensorflow import TensorFlowModel
 import os
 from dotenv import load_dotenv
 
+# todo: you want logging in here?
+# integrate deploy_model however
+
+'''def deploy_model_to_sagemaker(bucket_name, model_file, role): # move to deploy.py?
+    logger = setup_logging()
+    logger.info("Creating SageMaker model and deploying endpoint")
+
+    sagemaker_session = sagemaker.Session()
+    model = TensorFlowModel(
+        model_data=f's3://{bucket_name}/{model_file}',
+        role=role,
+        framework_version='2.3.0',
+        sagemaker_session=sagemaker_session
+    )
+
+    predictor = model.deploy(
+        initial_instance_count=1,
+        instance_type='ml.m5.large'
+    )
+    logger.info("Deployment complete")
+    return predictor'''
+
 load_dotenv(dotenv_path='.env')
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
-
 class Deploy:
-    def __init__(self, bucket_name, role, framework_version='2.18.0'):
+    def __init__(self):
         self.s3 = boto3.client('s3')
         self.bucket_name = os.getenv('MODEL_BUCKET_NAME')
         self.role = os.getenv('AWS_IAM_ROLE')
-        self.framework_version = framework_version
+        self.framework_version = '2.18.0'
         self.model_file = 'model.keras'
+        self.s3_upload = self.s3_upload()
+        self.deploy_model = self.deploy_model()
 
-    def upload_to_s3(self):
-        self.logger.info(f"Uploading {self.model_file} to S3 bucket {self.bucket_name}")
+    def s3_upload(self):
         self.s3.upload_file(self.model_file, self.bucket_name, self.model_file)
-        self.logger.info("Upload complete")
 
     def deploy_model(self):
-        self.logger.info("Creating SageMaker model and deploying endpoint")
         sagemaker_session = sagemaker.Session()
         model = TensorFlowModel(
             model_data=f's3://{self.bucket_name}/{self.model_file}',
@@ -38,10 +53,7 @@ class Deploy:
             initial_instance_count=1,
             instance_type='ml.m5.large'
         )
-        self.logger.info("Deployment complete")
         return predictor
 
     def delete_endpoint(self, predictor):
-        self.logger.info("Deleting SageMaker endpoint")
         predictor.delete_endpoint()
-        self.logger.info("Endpoint deleted") 
